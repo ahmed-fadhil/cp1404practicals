d59229be1d0b1e901813cdf050b58411b58385bb
ahmedfadhil <ahmedfadhil83@gmail.com>
2024-10-28 11:03:10 +1000

    Add lists warmup code

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,23 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0] # 3
+numbers[-1] # 2
+numbers[3] # 1
+numbers[:-1] #
+numbers[3:4]
+5 in numbers
+7 in numbers
+"3" in numbers
+numbers + [6, 5, 3] #
+
+# 1.Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = 'ten'
+
+# 2. Change the last element of numbers to 1
+numbers[-1] = 1
+
+# 3. Print all the elements from numbers except the first two (slice)
+
+
+# 4. Print whether 9 is an element of numbers
